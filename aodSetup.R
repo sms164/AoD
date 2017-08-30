540d1455a663acb3298d42b25350315e945bbc10
sms164 <sarah.mar.sullivan@gmail.com>
2017-08-30 17:06:16 -0400

    prep to create lagged/leading variables

--- aodSetup.R
+++ aodSetup.R
@@ -1,29 +1,30 @@
 #load packages
 install.packages("googlesheets")
 install.packages("Hmisc")
+install.packages("data.table")
 
 library("googlesheets")
 library("Hmisc")
+library(data.table)
 
 #Import data
 aodimport<-gs_title("Analysis of Demand")
 aod<-gs_read(aodimport, ws=4)
-ad<-as.data.frame(aod)
 
 #Explore data
 describe(aod)
 
 #Create Simple Calculated Variables
-
 aod$tabReq<-aod$trtAppr*2.8
 aod$tabRec<-aod$botRec*500
 aod$calcTabReq<-ifelse(is.na(aod$tabReq)==T, aod$tabShip, aod$tabReq)
 aod$tabAvail<-aod$tabInStock+aod$calcTabReq
-aod$diffTrt
+aod$diffTrtJA<-aod$popTarg-aod$trtAppr
 
 
+#Create variables dependent on next or previous year
 tabRem[i]<-tabInStock[i+1]
 pctRem<-tabRem/tabAvail
 tabChg1y[i]<-tabReq[i]-tabReq[i-1]
 pctChg1y[i]<-tabChg1y[i]/tabReq[i-1]
-diffTrtJA<-popTarg-trtAppr
+
