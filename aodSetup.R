7ad056156f0ee75643f4ceb1ae50f62ad12c97c8
sms164 <sarah.mar.sullivan@gmail.com>
2017-08-30 18:09:58 -0400

    completed lagged variables

--- aodSetup.R
+++ aodSetup.R
@@ -5,11 +5,13 @@ install.packages("data.table")
 
 library("googlesheets")
 library("Hmisc")
-library(data.table)
+library("data.table")
 
 #Import data
 aodimport<-gs_title("Analysis of Demand")
-aod<-gs_read(aodimport, ws=4)
+ad<-gs_read(aodimport, ws=4)
+aood<-ad[order(ad$country, ad$yearTrt), ]
+aod<-as.data.table(aood)
 
 #Explore data
 describe(aod)
@@ -23,8 +25,25 @@ aod$diffTrtJA<-aod$popTarg-aod$trtAppr
 
 
 #Create variables dependent on next or previous year
-tabRem[i]<-tabInStock[i+1]
-pctRem<-tabRem/tabAvail
-tabChg1y[i]<-tabReq[i]-tabReq[i-1]
-pctChg1y[i]<-tabChg1y[i]/tabReq[i-1]
+aod<-aod[, tabRem:=shift(tabInStock,n=1, fill=NA, type="lead"), by=country]
+aod$pctRem<-aod$tabRem/aod$tabAvail
+aod<-aod[, tabReq1ya:=shift(tabReq,n=1, fill=NA, type="lag"), by=country]
+aod$tabChg1y<-aod$tabReq-aod$tabReq1ya
+aod$pctChg1y<-aod$tabChg1y/aod$tabReq1ya
+
+#Dependent Variable Definitions
+#tabRem[i]<-tabInStock[i+1]
+#pctRem<-tabRem/tabAvail
+#tabChg1y[i]<-tabReq[i]-tabReq[i-1]
+#pctChg1y[i]<-tabChg1y[i]/tabReq[i-1]
+
+
+
+
+#Create variable to assess consistency of requests, usage and reported leftover
+
+#Create variable to assess the estimated number of treatments based on leftovers
+
+
+
 
